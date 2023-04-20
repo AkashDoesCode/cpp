88e8aa4a25e7bd55252ae7fd0989f6fdd35bc8c0
AkashDoesCode <131293814+AkashDoesCode@users.noreply.github.com>
2023-04-20 10:07:09 +0000

    success

--- Mathematics/digit_count.cpp
+++ Mathematics/digit_count.cpp
@@ -1,7 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-
+int main()
+{
+    int digit;
+    cout<<"enter digit"<<endl;
+    cin>>digit;
+    int res=0;
+    while(digit>0)
+    {
+        digit=digit/10;
+        res++;
+    }
+    cout<<"No of digits is"<<res<<endl;
     return 0;
 }
